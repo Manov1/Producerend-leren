90ddfa337b2b3eaac1e45750beb89a73d2d0181a
Nova <maritheitlager@gmail.com>
2023-03-22 10:25:42 +0100

    Added favicon and seperate header file

--- dist/klant.php
+++ dist/klant.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="en" class="bg-[#FEF9EF]">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>klant</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php
+include "header.php";
+?>
+<title>Klant</title>
 
 <body>
     <div>
--- dist/newuser.php
+++ dist/newuser.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" class="bg-[#FEF9EF]">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New user</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+
+<?php
+include "header.php";
+?>
+<title>New user</title>
+
 <body>
 <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div class="w-full max-w-md space-y-8">
